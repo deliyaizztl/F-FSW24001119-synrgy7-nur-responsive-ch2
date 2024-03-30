0db73cc4ab433323b1fa6a5673de0f7dbefe7ee5
deliyaizztl <deliyaizztl@gmail.com>
2024-03-30 17:59:05 +0700

    30/3/24 jam 5.58

--- custom.js
+++ custom.js
@@ -4,6 +4,29 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerGroup: 1, 
     loop: true,
     loopFillGroupWithBlank: true,
+    breakpoints: {
+        1000: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+        },
+
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        484: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        280: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        100: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+    },
 
     navigation: {
       nextEl: ".swiper-button-next",
